6f4abc206df913d66dbf5f6eced56e293d4d5175
Olaniyi toluwase <olaniyitoluwase799@gmail.com>
2025-11-17 17:54:51 +0100

    first commit

---
+++ project.js
@@ -0,0 +1,34 @@
+const slider = document.querySelector(".slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let counter = 0;
+const cardWidth = 89; 
+const visibleCards = -4;
+const totalCards = document.querySelectorAll(".digital").length;
+
+nextBtn.addEventListener("click", () => {
+  if (counter < totalCards - visibleCards) {
+    counter++;
+    updateSliderPosition();
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (counter > 0) {
+    counter--;
+    updateSliderPosition();
+  }
+});
+
+function updateSliderPosition() {
+  slider.style.transform = `translateX(-${counter * cardWidth}px)`;
+}
+
+
+
+
+window.addEventListener('load', () => {
+  document.getElementById('spinner').style.display = 'none';
+})
+
